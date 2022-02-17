8f8385ddcfd82f7f5fd89f17a37d2071b52f4e34
ryan brenon <rbrenon@gmail.com>
2022-02-16 19:51:10 -0500

    day 4 complete

--- day_4/passwords.py
+++ day_4/passwords.py
@@ -5,17 +5,16 @@
 #
 # 273025-767253
 
-from collections import Counter
-
 
 def main():
-    possible_pwd = list(range(273025, 767253))
-    # part_one(possible_pwd)
-    part_two(possible_pwd)
+    possible_pwd = list(range(273025, 767254))
+    part_one(possible_pwd)
+    # part_two(possible_pwd)
 
 
 def part_one(possible_pwd: list) -> None:
     valid_pwd = 0
+    valid_pwd_list = []
     for pwd in possible_pwd:
         if (
             int(str(pwd)[0])
@@ -32,37 +31,28 @@ def part_one(possible_pwd: list) -> None:
             or (int(str(pwd)[4]) == int(str(pwd)[5]))
         ):
             valid_pwd += 1
+            valid_pwd_list.append(pwd)
 
-    print(f'Part 1 Valid Passwords: {valid_pwd}')  # 910
+    print(f"Part 1 Valid Passwords: {valid_pwd}")  # 910
+    part_two(valid_pwd_list)
 
 
 def part_two(possible_pwd: list) -> None:
-    valid_pwd = 0
+    valid_pwd_2 = 0
+    valid_pwd_2_list = []
     for pwd in possible_pwd:
-        if (
-                int(str(pwd)[0])
-                <= int(str(pwd)[1])
-                <= int(str(pwd)[2])
-                <= int(str(pwd)[3])
-                <= int(str(pwd)[4])
-                <= int(str(pwd)[5])
-        ) and (
-                (int(str(pwd)[0]) == int(str(pwd)[1]) and int(str(pwd)[1]) < int(str(pwd)[2]))
-                or (int(str(pwd)[1]) == int(str(pwd)[2]) and int(str(pwd)[2]) < int(str(pwd)[3]))
-                or (int(str(pwd)[2]) == int(str(pwd)[3]) and int(str(pwd)[3]) < int(str(pwd)[4]))
-                or (int(str(pwd)[3]) == int(str(pwd)[4]) and int(str(pwd)[4]) < int(str(pwd)[5]))
-                or (int(str(pwd)[4]) == int(str(pwd)[5]))
-        ):
-            counter = Counter
-            max_dupes = [char.count(char) for char in str(pwd)]
-            print(max_dupes)
-            # if max_dupes < 3:
-            #     print(pwd)
-            #     valid_pwd += 1
+        unique_numbs = set(str(pwd))
+        occurrences = {}
+        for numb in unique_numbs:
+            occurrences[int(numb)] = str(pwd).count(numb)
+        if 2 in occurrences.values():
+            valid_pwd_2_list.append(pwd)
+            valid_pwd_2 += 1
 
-    print(f'Part 2 Valid Passwords: {valid_pwd}')  # 910
+    print(
+        f"Part 2 Valid Passwords: {valid_pwd_2}"
+    )  # 251, 475 - too low; 1603 - too high; 598 - correct
 
 
 if __name__ == "__main__":
     main()
-
